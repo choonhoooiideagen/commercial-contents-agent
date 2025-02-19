3ac05ab97c189990406eed834b19893605dbc5b3
IDEAGENPLC\zharfan.zahisham <zharfan.zahisham@ideagenplc.com>
2025-02-19 11:57:57 +0800

    Add new files for metadata tagging

---
+++ docs-keeper-agent/utils/utils.py
@@ -0,0 +1,57 @@
+from PyPDF2 import PdfReader
+from io import BytesIO
+import json
+
+
+def pdf_bytes_to_text(pdf_bytes: bytes) -> str:
+    """
+    Converts the PDF bytes to text.
+    Args:
+        pdf_bytes (bytes): The PDF bytes to convert to text.
+    Returns:
+        str: The text extracted from the PDF.
+    """
+    reader = PdfReader(BytesIO(pdf_bytes))
+    contents = ''
+
+    for page in reader.pages:
+        contents += page.extract_text()
+    
+    return contents
+
+
+def clean_json_string(json_string: str) -> str:
+    """
+    Cleans the JSON string by removing the escape characters.
+    Args:
+        json_string (str): The JSON string to clean.
+    Returns:
+        str: The cleaned JSON string.
+    """
+    json_string = json_string.replace("```json", "")
+    json_string = json_string.replace("```", "")
+    json_string = json_string.strip()
+
+    return json_string
+
+
+def write_to_db(data: dict) -> bool:
+    """
+    Writes the data to the database.
+    Args:
+        data (dict): The data to write to the database.
+    Returns:
+        bool: True if the data was successfully written, False otherwise.
+    """
+    print("WRITING INTO DB:", data)
+
+    # Load the existing data
+    with open("metadata.json", "r") as f:
+        existing_data = json.load(f)
+
+    # Append the new data
+    existing_data.append(data)
+
+    # Write the data back to the database
+    with open("metadata.json", "w") as f:
+        json.dump(existing_data, f, indent=2) 
