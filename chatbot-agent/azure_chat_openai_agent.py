05127fbb552ca2f70525bfe0de23e14db9608efc
Choon Ho Ooi <choonho.ooi@ideagenplc.com>
2025-02-19 15:28:10 +0800

    ﻿Improvised the simple chatbot

--- chatbot-agent/azure_chat_openai_agent.py
+++ chatbot-agent/azure_chat_openai_agent.py
@@ -1,17 +1,21 @@
-import json
 from langchain_openai import AzureChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
-from config import azure_openai_api_key, azure_openai_endpoint, azure_openai_deployment, azure_openai_api_version
-from utils.string_utils import clean_json_string
+from chroma_knowledge_base_agent import ChromaKnowledgeBaseAgent
+# from utils.string_utils import clean_json_string
+from dotenv import load_dotenv
+# import json
+import os
+
+load_dotenv()
 
 class AzureChatOpenAIAgent:
-    def converse(user_info, user_input, user_input_history):
+    def converse(user_info, user_prompt, user_prompt_history):
         llm = AzureChatOpenAI(
-            api_key=azure_openai_api_key,
-            azure_endpoint=azure_openai_endpoint,
-            azure_deployment=azure_openai_deployment,
-            api_version=azure_openai_api_version,
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+            api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
             temperature=0,
             max_tokens=None,
             timeout=None,
@@ -19,11 +23,14 @@ class AzureChatOpenAIAgent:
         )
 
         response_schemas = [
-            ResponseSchema(name="Quote", description="Phrase from the knowledge base sources"),
-            ResponseSchema(name="Source", description="Source of the quote"),
+            ResponseSchema(name="id", description="ID of the content"),
+            ResponseSchema(name="title", description="Title of the content"),
+            ResponseSchema(name="summary", description="Summary of the content"),
+            ResponseSchema(name="price", description="Price of the content"),
         ]
 
-        knowledge_base = "Fire safety in a company is about having extinguisher, fire alarms, and fire drills."
+        knowledge_base_agent = ChromaKnowledgeBaseAgent
+        knowledge_base = knowledge_base_agent.retrieve(user_prompt)
         output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
         prompt = ChatPromptTemplate.from_messages(
@@ -31,14 +38,33 @@ class AzureChatOpenAIAgent:
                 (
                     "system",
                     """
+                    You are provided a knowledge base that consists of "{knowledge_base}".
+                    """
+                ),
+                (
+                    "system",
+                    """
                     You are a Content Management Agent for a large company.
-                    You are tasked with recommending the most relevant content from "{knowledge_base}" based on user input.
-                    "{output_format}"
+                    You are tasked with recommending relevant content from the provided knowledge base based on user prompt, provide a maximum of 5 recommendations.
+                    If the price of the content is not available, fabricate a value.
+                    You will need to also provide a short and suitable reply to the user prompt for the recommended content.
+                    If there is a mention about the recommended list of content in the response, ensure that the content is listed in using the following format:
+                    #contents-list
+                    {{
+                        "contents": [
+                            {{
+                                "id": 1,
+                                "title": "Learn Spanish",
+                                "price": 10
+                            }}
+                        ]
+                    }}
+                    #end-contents-list
                     """
                 ),
                 (
                     "user",
-                    "{user_input}"
+                    "{user_prompt}"
                 )
              ]
         )
@@ -47,11 +73,15 @@ class AzureChatOpenAIAgent:
         response = chain.invoke({
             "input_language": "English",
             "output_language": "English",
-            "user_input": user_input,
+            "user_prompt": user_prompt,
             "knowledge_base": knowledge_base,
-            "output_format": output_parser,
+            "output_format": output_parser.get_format_instructions(),
         })
 
-        response_clean = clean_json_string(response.content)
-        response_json = json.loads(response_clean)
-        print(response_json)
+        # print(response.content)
+        return response.content
+
+        # response_clean = clean_json_string(response.content)
+        # response_json = json.loads(response_clean)
+        # print(f"Answer: {response_json}\n")
+        # return response_json
