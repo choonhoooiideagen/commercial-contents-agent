592c46779a805bea9653557aa82555c21840e22d
Choon Ho Ooi <choonho.ooi@ideagenplc.com>
2025-02-19 11:17:53 +0800

    Checkpoint before switching branch

--- chatbot-agent/bedrock_chatbot.py
+++ chatbot-agent/bedrock_chatbot.py
@@ -1,10 +1,11 @@
 import boto3
 import uuid
-from config import model_id, agent_id, agent_alias_id, aws_profile, aws_region
+import json
+from config import aws_profile, aws_region, model_id, agent_id, agent_alias_id
 from chatbot_system_prompts import content_management_system_prompt
 
 # Initialize a session using a specific profile
-session = boto3.Session(profile_name=aws_profile,region_name=aws_region)
+session = boto3.Session(profile_name = aws_profile,region_name=aws_region)
 
 # Initialize the Bedrock client
 bedrock_client = session.client('bedrock-runtime')
@@ -17,9 +18,9 @@ def converse_with_model(user_query):
     ]
 
     response = bedrock_client.converse(
-        modelId=model_id,
-        messages=messages,
-        system=[content_management_system_prompt],
+        modelId = model_id,
+        messages = messages,
+        system = [content_management_system_prompt],
     )
     return response['output']['message']['content'][0]['text']
 
@@ -27,11 +28,16 @@ def converse_with_model(user_query):
 # Replace with your actual agent ID and knowledge base IDagent_id = "your-agent-id"session_id = "your-session-id" 
 # Use a unique session ID for each conversation
 def invoke_agent(user_input):
-    response = bedrock_agent_client.invoke_agent(agentAliasId=agent_alias_id, agentId=agent_id, sessionId=str(uuid.uuid4()), inputText=user_input)
+    response = bedrock_agent_client.invoke_agent(
+        agentAliasId = agent_alias_id,
+        agentId = agent_id,
+        sessionId = str(uuid.uuid4()),
+        inputText = user_input)
 
     # Yield streamed events and capture response text
     responses = []
     for event in response["completion"]:
+        # print(event)
         responses.append({
             "text": event["chunk"]["attribution"]["citations"][0]["generatedResponsePart"]["textResponsePart"]["text"]
         })
